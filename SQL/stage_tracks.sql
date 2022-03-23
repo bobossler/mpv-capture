ab127f0811025881ad5796896dc0dd31689b8955
Bob Ossler <bob.ossler@gmail.com>
2022-03-23 10:11:20 -0500

    added table create sql

---
+++ SQL/stage_tracks.sql
@@ -0,0 +1,1 @@
+CREATE TABLE stage_tracks(artist text, song text);
---
+++ SQL/tunes.sql
@@ -0,0 +1,1 @@
+CREATE TABLE tunes(artist text, song text, create_ts text default  CURRENT_DATE, maint_ts  text default CURRENT_DATE);
